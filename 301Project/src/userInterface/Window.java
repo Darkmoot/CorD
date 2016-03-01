34beb8f6bd330eade5c60c960570f35b1ea6c68d
Darkmoot <kenweilee@gmail.com>
2016-03-01 15:20:59 -0500

    Moved zombie into own graphics source folder
    Added an enemy to the game window to test painting

--- 301Project/src/userInterface/Window.java
+++ 301Project/src/userInterface/Window.java
@@ -7,6 +7,7 @@ import java.awt.event.WindowEvent;
 
 import javax.swing.text.BadLocationException;
 
+import backEnd.Enemy;
 import backEnd.GameArea;
 
 /**
@@ -138,6 +139,9 @@ public class Window extends javax.swing.JFrame {
             GameAreaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 0, Short.MAX_VALUE)
         );
+        
+		gArea.addEnemy(new Enemy(10, 10));
+		gArea.repaint();		
 
         QuestionText.setColumns(20);
         QuestionText.setRows(5);
