5c8aa72f6ab86ea148fa9f86410c4cc5124d0349
g4laura <Laura.garrett@mail.utoronto.ca>
2016-04-02 16:12:01 -0400

    Update Game.java

--- 301Project/src/Game.java
+++ 301Project/src/Game.java
@@ -72,8 +72,9 @@ public class Game {
 					// clear stuff of screen here to start new level
 					// also curently just using same question types and lesson
 					//w.setGameArea();
-					tt = Arrays.asList(levelTypes[level]);
+					
 					level++;
+					tt = Arrays.asList(levelTypes[level]);
 					curLevel = lgen.createLevel(new Lesson(0, 0, lessonPaths[level]), tt);
 					
 					times = curLevel.startLevel();
